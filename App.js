46fb309cf078c3c89e3f40e19f214fe1eef6e061
Derek Zhou <dzhou84@gatech.edu>
2021-10-21 20:44:59 -0400

    touchable opacity

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <View style={styles.title}>
-        <Text style={{fontSize: 40,}}> Welcome to GymTracker! </Text>
+        <Text style={{ fontSize: 40, }}> Welcome to GymTracker! </Text>
       </View>
-      <View style= {{padding: 30,}}>
+      <View style={{ padding: 30, }}>
         <Text> Username or Email </Text>
-        <TextInput placeholder="johndoe@example.com" style = {styles.input}/>
+        <TextInput placeholder="johndoe@example.com" style={styles.input} />
       </View>
-      <View style= {{padding: 30,}}>
+      <View style={{ padding: 30, }}>
         <Text> Password </Text>
-        <TextInput placeholder="Must have at least 5 characters" style = {styles.input}/>
+        <TextInput placeholder="Must have at least 5 characters" style={styles.input} />
       </View>
-      <View style= {{padding: 30,}}>
-        <Button style={{padding:10, fontSize: 20}} title="Sign In"/>
-        <Button style={{padding:10, fontSize: 20}} title="Create an Accounnt"/>
-      </View>
+      <TouchableOpacity>
+        <View style={{ padding: 30, }}>
+          <Button style={{ padding: 10, fontSize: 20 }} title="Sign In" />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={{ padding: 30, }}>
+          <Button style={{ padding: 10, fontSize: 20 }} title="Create an Accounnt" />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -33,7 +39,7 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   input: {
-    borderBottomColor: 'black', 
+    borderBottomColor: 'black',
     borderBottomWidth: 1,
   }
 });
