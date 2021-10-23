38c593dcdea340339e17ae790e594f4e353230cd
Derek Zhou <dzhou84@gatech.edu>
2021-10-23 14:10:03 -0400

    separate screens

--- App.js
+++ App.js
@@ -1,57 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  return (
-    <View style={styles.screen}>
-      <View style={styles.title}>
-        <Text style={{ fontSize: 40, }}> Welcome to GymTracker! </Text>
-      </View>
-      <View style={{ padding: 30, }}>
-        <Text> Username or Email </Text>
-        <TextInput
-        placeholder="johndoe@example.com"
-        style={styles.input}
-        onChangeText={text => setUsername(text)}/>
-      </View>
-      <View style={{ padding: 30, }}>
-        <Text> Password </Text>
-        <TextInput
-        placeholder="Must have at least 5 characters" 
-        style={styles.input}
-        onChangeText={text => setPassword(text)} />
-      </View>
-      <TouchableOpacity>
-        <View style={{ padding: 30, }}>
-          <Button
-          style={{ padding: 10, fontSize: 20 }}
-          title="Sign In"
-          // for debugging 
-          onPress={() => console.log(`${username} ${password}`)} />
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View style={{ padding: 30, }}>
-          <Button style={{ padding: 10, fontSize: 20 }} title="Create an Accounnt" />
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+  return <WelcomeScreen/>;
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
-  },
-  title: {
-    alignItems: 'center',
-    padding: 30,
-  },
-  input: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-  }
+  
 });
---
+++ components/WelcomeScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+
+function WelcomeScreen(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.screen}>
+      <View style={styles.title}>
+        <Text style={{ fontSize: 40, }}> Welcome to GymTracker! </Text>
+      </View>
+      <View style={{ padding: 30, }}>
+        <Text> Username or Email </Text>
+        <TextInput
+        placeholder="johndoe@example.com"
+        style={styles.input}
+        onChangeText={text => setUsername(text)}/>
+      </View>
+      <View style={{ padding: 30, }}>
+        <Text> Password </Text>
+        <TextInput
+        placeholder="Must have at least 5 characters" 
+        style={styles.input}
+        onChangeText={text => setPassword(text)} />
+      </View>
+      <TouchableOpacity>
+        <View style={{ padding: 30, }}>
+          <Button
+          style={{ padding: 10, fontSize: 20 }}
+          title="Sign In"
+          // for debugging 
+          onPress={() => console.log(`${username} ${password}`)} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={{ padding: 30, }}>
+          <Button style={{ padding: 10, fontSize: 20 }} title="Create an Accounnt" />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  title: {
+    alignItems: 'center',
+    padding: 30,
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  }
+});
+
+export default WelcomeScreen;
