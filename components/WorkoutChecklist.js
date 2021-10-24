8a552e8db12bf61123b31ab73bec3e14872a0600
Derek Zhou <dzhou84@gatech.edu>
2021-10-23 21:18:01 -0400

    Checklist styling

--- components/WorkoutChecklist.js
+++ components/WorkoutChecklist.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, FlatList, SafeAreaView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function WorkoutChecklist() {
     // Will fetch these from the server once connected to backend
@@ -20,16 +24,18 @@ export default function WorkoutChecklist() {
 
   return(
       <SafeAreaView>
-          <Text>Workout {workoutNum}</Text>
-          <Text>{workoutName}</Text>
-          <View>
-              <Text>{date.getMonth()}/{date.getDay()}/{date.getFullYear()}</Text>
+          <Text style = {{fontSize: 35, fontWeight: 'bold', textAlign: 'center'}}> Workout {workoutNum}</Text>
+          <Text style = {{fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}> {workoutName}</Text>
+          <View style = {styles.checklist}>
+              <Text style = {styles.date}>{date.getMonth()}/{date.getDay()}/{date.getFullYear()}</Text>
               <FlatList
               data={exercises}
               renderItem={({ item }) => {
                   return(
-                      <View>
-                          <Text>{item.name}</Text>
+                      <View style = {{flexDirection:'row', paddingTop: 30, justifyContent: 'space-between'}}>
+                          <View>
+                            <Text>{item.name}</Text>
+                          </View>
                           <TouchableOpacity
                           onPress={() => {
                               const idx = exercises.indexOf(item);
@@ -37,13 +43,19 @@ export default function WorkoutChecklist() {
                               newExercises.splice(idx, 1, {name: item.name, completed: !item.completed});
                               setExercises(newExercises);
                           }}>
-                              {displayIcon(item.completed)}
+                              <View style = {{alignItems: 'flex-end'}}>
+                                {displayIcon(item.completed)}
+                              </View>
                           </TouchableOpacity>
                       </View>
                   )
               }} />
           </View>
-          <Button title="Back"/>
+          <TouchableOpacity> 
+            <View style = {styles.back}>
+                <Text style = {{fontSize: 25, textAlign: 'center', }}> Back </Text>
+            </View>
+        </TouchableOpacity>  
       </SafeAreaView>
   );
 }
@@ -51,5 +63,25 @@ export default function WorkoutChecklist() {
 const styles = StyleSheet.create({
     container: {
         fontSize: 35
+    },
+    checklist: {
+        padding: 20,
+        marginVertical: 10,
+        backgroundColor: 'lightgray',
+        borderColor: 'black',
+        borderRadius: 25,
+        alignContent: 'space-between',
+    },
+    date: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    back: {
+        padding: 20,
+        marginVertical: 10,
+        backgroundColor: 'pink',
+        borderColor: 'black',
+        borderRadius: 25,
     }
 });
